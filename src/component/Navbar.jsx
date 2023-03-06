035e3e97408e778ab6b43cc203fc6af59fee5382
nidamuna <munanida@gmail.com>
2023-03-06 15:27:58 +0700

    responsive navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {CiMenuKebab} from 'react-icons/ci'
+import {IoCloseOutline} from 'react-icons/io5'
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
+
+    const handleNav = () => {
+        setNavbar(!navbar);
+    }
 
   return (
     <>
-        <nav className='w-full bg-white h-auto py-4 px-8 flex justify-between items-center'>
+        <nav className='w-full bg-white h-[75px] py-4 px-8 flex justify-between items-center'>
             <span className='bg-blue-500 p-2 text-white'>
                 Logo Pelatihan
             </span>
             <div className='font-bold p-2 text-[#212529]'>
-                <ul className='flex flex-row cursor-pointer'>
-                    <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Home</li>
+                <ul className='hidden md:flex flex-row cursor-pointer'>
+                    <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Tentang Kami</li>
                     <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Pelatihan</li>
                     <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Fasilitas</li>
-                    <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Contact Us</li>
+                    <li className='mr-10 last:mr-0 hover:text-[#0a58ca] hover:underline'>Kontak Kami</li>
                 </ul>
             </div>
-            <button className='bg-[#6136ff] hover:bg-[#0a58ca] rounded-full text-white font-semibold p-2 shadow-md'>
+            <button className='hidden md:flex bg-[#6136ff] hover:bg-[#0a58ca] rounded-full text-white font-semibold p-2 shadow-md'>
                 Daftar Sekarang
             </button>
+            {/* phone */}
+            <CiMenuKebab 
+                className={navbar ? 'hidden' : 'flex'}
+                onClick={handleNav}
+                style={{ height: '30px', width: '20px', color: '#120036'}}
+            />
+            <IoCloseOutline 
+                className={!navbar ? 'hidden' : 'flex'}
+                onClick={handleNav}
+                style={{ height: '30px', width: '20px', color: '#120036'}}
+            />
         </nav>
+        <div className={navbar ? 'w-full h-auto p-5 bg-white shadow-lg' : 'hidden'}>
+            <ul className='flex flex-col cursor-pointer'>
+                <li className='mb-4 last:mb-0 hover:text-[#0a58ca] hover:underline'>Tentang Kami</li>
+                <li className='mb-4 last:mb-0 hover:text-[#0a58ca] hover:underline'>Pelatihan</li>
+                <li className='mb-4 last:mb-0 hover:text-[#0a58ca] hover:underline'>Fasilitas</li>
+                <li className='mb-4 last:mb-0 hover:text-[#0a58ca] hover:underline'>Kontak Kami</li>
+            </ul>
+            <button className='mt-8 w-full bg-[#6136ff] hover:bg-[#0a58ca] rounded-full text-white font-semibold p-2 shadow-md'>
+                Daftar Sekarang
+            </button>
+        </div>
     </>
   )
 }
