00c86e3a1081519ccad6e799f09a4b0caca93c50
nidamuna <munanida@gmail.com>
2023-03-06 15:08:14 +0700

    add screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '862px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+    }
   },
   plugins: [],
 }
