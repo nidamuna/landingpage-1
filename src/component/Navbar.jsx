9137da5dc8b5060fac9520ee77cc9540495af6f3
nidamuna <munanida@gmail.com>
2023-03-06 11:06:01 +0700

    navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <nav className='w-full bg-white h-auto py-4 px-8 flex justify-between items-center shadow-lg'>
+            <span className='bg-blue-500 p-2 text-white'>
+                Logo Pelatihan
+            </span>
+            <div className='font-bold p-2 text-[#212529]'>
+                <ul className='flex flex-row cursor-pointer'>
+                    <li className='mr-10 last:mr-0 hover:text-blue-500 hover:underline'>Home</li>
+                    <li className='mr-10 last:mr-0 hover:text-blue-500 hover:underline'>Pelatihan</li>
+                    <li className='mr-10 last:mr-0 hover:text-blue-500 hover:underline'>Fasilitas</li>
+                    <li className='mr-10 last:mr-0 hover:text-blue-500 hover:underline'>Contact Us</li>
+                </ul>
+            </div>
+            <button className='bg-blue-500 rounded-full text-white font-semibold p-2 shadow-md'>
+                Daftar Sekarang
+            </button>
+        </nav>
+    </>
   )
 }
 
