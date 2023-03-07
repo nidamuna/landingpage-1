0ccac0001f34e41f71c516878b7c7395ae556eef
nidamuna <munanida@gmail.com>
2023-03-07 15:01:35 +0700

    add hero image

--- src/component/Herosection.jsx
+++ src/component/Herosection.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import doctor from '../assets/doctor.png'
+import doctor from '../assets/doctor2.png'
 
 const Herosection = () => {
   return (
     <>
-      <section id='hero' className='w-full bg-[#8ECAE6] lg:min-h-0 min-h-screen text-[#023047] pt-[160px] px-10 pb-[100px]'>
-        <div className='flex flex-col-reverse md:flex-row container mx-auto'>
+      <section id='hero' className='w-full bg-[#8ECAE6] lg:min-h-0 min-h-screen text-[#023047] pt-[60px] pl-10 pb-20 md:pb-0 pr-10 md:pr-0'>
+        <div className='flex flex-col-reverse md:flex-row justify-center items-center'>
           {/* Left */}
-          <div className='w-full md:w-[50%]'>
+          <div className='w-full md:w-[50%] flex flex-col items-end'>
             <h1 className='font-black text-[2.875rem]'>
               Pelatihan Sunat Modern
             </h1>
-            <div className='bg-[#fcfdff] py-3 px-5 mt-6 rounded-md text-[19px] font-extralight'>
+            <div className='bg-[#fcfdff] max-w-[550px] p-8 mt-6 rounded-2xl text-[19px] font-extralight'>
               <p>
                 Bergabunglah dengan ribuan peserta yang telah memperluas pengetahuan 
                 mereka di 
-                <br/>
-                <strongs className='stabs font-semibold'>
+                <strongs className='stabs font-semibold ml-1'>
                   bidang sunat modern
                 </strongs>
               </p>
@@ -32,14 +31,15 @@ const Herosection = () => {
             </div>
           </div>
           {/* Right */}
-          <div className='w-full md:h-[400px] overflow-hidden md:w-[50%] ml-0 md:ml-11 bg-red-100 mb-10 md:mb-0'>
-            <div className='p-10 rounded-full'>
-              <img src={doctor} alt="doctor" className='h-[500px]' />
+          <div className='w-full md:h-[500px] overflow-hidden md:w-[50%] ml-0 md:ml-11 mb-10 md:mb-0'>
+            <div className='w-full flex items-end lg:items-start h-[300px] md:h-[500px]'>
+              <img src={doctor} alt="doctor" className='' />
             </div>
           </div>
         </div>
-        <div className='text-center mt-20 text-[16px] font-light leading-5 flex flex-col justify-center items-center'>
-          <p className='w-full md:w-[70%] max-w-[1000px]'>
+      </section>
+      <div className='bg-white text-center pt-20 text-[16px] font-light leading-5 flex flex-col justify-center items-center'>
+          <p className='w-full max-w-[1000px]'>
             Pelatihan sunat adalah program pelatihan yang dirancang untuk memberikan 
             pengetahuan, keterampilan, dan pemahaman tentang teknik sunat yang benar dan aman. 
             <span className='stabs'>Tujuan utama dari pelatihan sunat adalah untuk meningkatkan kualitas 
@@ -50,7 +50,6 @@ const Herosection = () => {
             pasien yang menjalani prosedur sunat.
           </p>
         </div>
-      </section>
     </>
   )
 }
